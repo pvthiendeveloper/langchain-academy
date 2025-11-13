e7fcee05c6d4658f24bfda2b0ec9df3f29d73f10
Thien Pham <pvthiendeveloper@gmail.com>
2025-11-14 01:57:46 +0700

    chore: add practice router

---
+++ module-1/studio/practice_router.py
@@ -0,0 +1,35 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+
+llm = ChatOpenAI(model="gpt-4o")
+llm_with_tools = llm.bind_tools([multiply])
+
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges("tool_calling_llm", tools_condition)
+builder.add_edge("tools", END)
+
+# Compile graph
+graph = builder.compile()
